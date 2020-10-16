1e7d0ea6692bfa3e2fdbd0a868703543ccb9d9e3
yashpatel123a <Yashpatel@97248>
2020-10-16 09:55:46 +0530

    code cleanup

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     //| 6 | 7 | 8 |
     //+---+---+---+
 
-    int [][] winningPositions = {   {0, 1, 2}, 
+    int [][] winningPositions = {   {0, 1, 2},
                                     {3, 4, 5},
                                     {6, 7, 8},
                                     {0, 3, 6},
